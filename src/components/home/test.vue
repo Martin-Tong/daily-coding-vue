ebfc3d597356ecbb1d5152d278c5c1f514cf763e
trk19931112 <1933981377@qq.com>
2020-06-11 23:47:08 +0800

    customEvent chain call

--- src/components/home/test.vue
+++ src/components/home/test.vue
@@ -7,7 +7,19 @@
 */
 
 //<script>
+const Test = () => import('../../../utils/customeEvent.js')
 export default {
-
+    mounted() {
+        console.log(Test())
+        let a =document.querySelector('#nest2')
+        a.addEventListener('test',()=>{alert(1)})
+        this.test()
+    },
+    methods: {
+        test() {
+            let a =document.querySelector('#nest2')
+            Test().then((e) => e.default.create('test', {bubbles: true, cancelable: true, detail: 'test data'}).trigerEvent(a, 'test'))
+        }
+    }
 }
 //</script>
--- utils/customeEvent.js
+++ utils/customeEvent.js
@@ -1,7 +1,8 @@
 // @flow
 let handle: {
     events: ?{[key: string]: Array<Object>},
-    create(type: string, options: CustomEvent$Init): Object
+    create(type: string, options: CustomEvent$Init): Object,
+    trigerEvent(target: Object, eventType: string): Object
     } = {
         events: {},
         create(type, options) {
@@ -12,9 +13,26 @@ let handle: {
             } else {
                 events[type] = [ins]
             }
-            return ins
+            return this
+        },
+        trigerEvent(target, eventType) {
+            if ('dispatchEvent' in target && eventType in this.events) {
+                let events: Array<Object> = this.events[eventType],
+                    length: number = events.length - 1
+                do {
+                    target.dispatchEvent(events[length])
+                } while (length-- > 0)
+            } else if ('dispatchEvent' in target && true) {
+                throw new Error(`Make sure ${eventType} has been register correctly.`)
+            } else if (eventType in this.events && true) {
+                throw new Error(`Make sure ${Object.prototype.toString.call(target)} has "dispatchEvent" method.`)
+            } else {
+                throw new Error(
+                    `Make sure ${target} has "dispatchEvent" method.\nMake sure ${eventType} has been register correctly.`
+                )
+            }
+            return this
         }
-
     }
 
 export default handle
